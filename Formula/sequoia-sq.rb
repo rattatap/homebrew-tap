18ce1ae5c551a33f02039535345dd6a67483b4cc
Ryan Rotter <rrotter@users.noreply.github.com>
2024-10-19 17:00:03 -0400

    sequoia-sq: fix deps, add tests, rename
    Builds and passes `brew linkage --test` on macOS and Linux. Rename because of conflict with an unrelated formula called `sq` in homebrew/core.

--- Formula/sequoia-sq.rb
+++ Formula/sequoia-sq.rb
@@ -1,20 +1,26 @@
-class Sq < Formula
+class SequoiaSq < Formula
   desc "Sequoia-PGP command-line tool"
   homepage "https://sequoia-pgp.org"
   url "https://gitlab.com/sequoia-pgp/sequoia-sq/-/archive/v0.38.0/sequoia-sq-v0.38.0.tar.gz"
   sha256 "9fd32ad0de3388804e21205003821710d2faf99d5c85a50bd97da3e7e480921b"
   license "GPL-2.0-or-later"
+  head "https://gitlab.com/sequoia-pgp/sequoia-sq.git", branch: "main"
 
   depends_on "capnp" => :build
-  depends_on "nettle" => :build
+  depends_on "pkg-config" => :build
   depends_on "rust" => :build
+  depends_on "gmp"
+  depends_on "nettle"
+  depends_on "openssl@3"
 
   on_linux do
-    depends_on "openssl@3" # Uses Secure Transport on macOS
+    depends_on "llvm" => :build
+    depends_on "bzip2"
+    depends_on "sqlite"
   end
 
   def install
-    ENV["OPENSSL_DIR"] = Formula["openssl@3"].opt_prefix if OS.linux?
+    ENV["OPENSSL_DIR"] = Formula["openssl@3"].opt_prefix
     ENV["ASSET_OUT_DIR"] = buildpath
 
     system "cargo", "install", *std_cargo_args
@@ -26,8 +32,8 @@ class Sq < Formula
   end
 
   test do
-    # This isn't a test, it's a useless placeholder.
-    # If I had a test, I'd have submitted this formula to Homebrew.
-    true
+    assert_match version.to_s, shell_output("#{bin}/sq version 2>&1")
+    assert_match "R0lGODdhAQABAPAAAAAAAAAAACwAAAAAAQABAAACAkQBADs=",
+      shell_output("cat #{test_fixtures("test.gif")} | #{bin}/sq toolbox armor")
   end
 end
